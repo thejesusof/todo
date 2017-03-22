4fbf6ad9f8986cace9f83f16898bf8215a3b1fdf
thejesusof <thejesusof@gmail.com>
2017-03-22 23:26:57 +0300

    create toDoList object

--- main.js
+++ main.js
@@ -1,52 +1,58 @@
-var toDoList = [];
 var toDoListHTML = document.getElementById('to-do-list');
+
+var toDoList = {
+  
+  toDoListArr: [],
+  
+  add: function(listItem) {
+        this.toDoListArr.push(listItem);
+      },
+      
+  render: function() {
+            toDoListHTML.innerHTML = '';
+            for(var i = 0; i < this.toDoListArr.length; i++) {
+              var listItemObj = this.toDoListArr[i];
+              listItemObj.render();
+            }
+          },
+  
+  delete: function(titleToFind) {
+            for(var i = 0; i < this.toDoListArr.length; i++) {
+              if(this.toDoListArr[i].title == titleToFind) {
+                this.toDoListArr.splice(i, 1);
+              }
+            }
+          }
+};
+
 
 function ListItemObj(userInput) {
   this.title = userInput;
   this.status = 'new';
-  this.addListItemToArr = function() {
-                            toDoList.push(this);
-                          };
   this.render = function() {
                   var newLi = document.createElement('div');
                   newLi.className = "list-item";
                   newLi.innerHTML = "<span class=user-input-span>"+ userInput + "</span>" + "<button class=delete-list-item>Delete</button>";
                   toDoListHTML.appendChild(newLi);
                 };
-  this.deleteListItemFromDOM = function() {
-                                  var deleteListItemBtns = document.getElementsByClassName("delete-list-item");
-                                  for(var i = 0; i < deleteListItemBtns.length; i++) {
-                                  	deleteListItemBtns[i].addEventListener('click', function(){
-                                  	  var titleToFind = this.parentNode.parentNode.getElementsByTagName("span")[0].innerHTML;
-                                  	  deleteListItemFromObj(titleToFind);
-                                  		this.parentNode.parentNode.removeChild(this.parentNode);
-                                   });
-                                  }
-                                };
+  this.delete = function() {
+                        var deleteListItemBtns = document.getElementsByClassName("delete-list-item");
+                        for(var i = 0; i < deleteListItemBtns.length; i++) {
+                          deleteListItemBtns[i].addEventListener('click', function(){
+                            var titleToFind = this.parentNode.getElementsByTagName("span")[0].innerHTML;
+                            toDoList.delete(titleToFind);
+                            this.parentNode.parentNode.removeChild(this.parentNode);
+                         });
+                        }
+                      }
 }
 
 document.getElementById('user-input-submit').addEventListener('click', function(){
   var userInput = document.getElementById('user-input').value;
   if(userInput){
-    var newListItemObj = new ListItemObj(userInput);
-    newListItemObj.addListItemToArr();
-    newListItemObj.render();
-    newListItemObj.deleteListItemFromDOM();
+    var listItem = new ListItemObj(userInput);
+    toDoList.add(listItem);
+    toDoList.render();
+    listItem.delete();
   }
 });
-
-function deleteListItemFromObj(titleToFind) {
-  for(var i = 0; i < toDoList.length; i++) {
-      if(toDoList[i].title == titleToFind) {
-        toDoList.splice(i, 1);
-      }
-    }
-}
-
-function changeListItem(changedText, titleToFind) {
-  for(var i = 0; i < toDoList.length; i++) {
-    if(toDoList[i].title == titleToFind) {
-      toDoList[i].title = changedText;
-    }
-  }
-}
